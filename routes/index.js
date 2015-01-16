d8fd6326ded8b2cb6faac50b437acbe071cf4cbd
Aleksi Vuorjoki <aleksi.vuorjoki@gmail.com>
2015-01-16 04:57:36 +0200

    Added a form for submitting the url and edited the ui

--- routes/index.js
+++ routes/index.js
@@ -5,12 +5,18 @@ var router = express.Router();
 router.get('/:id', function (req, res, next) {
     console.log('redirecting permanently');
     //res.send('stuff');
-    res.redirect(301, 'http://reaktor.fi/careers/summerjob/');
+    res.redirect(301, "http://reaktor.fi/careers/summerjob/");
 });
+
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-    res.render('index', {title: 'OgreSound\'s url minifier'});
+    res.render('index');
+});
+
+router.post('/shorten', function (req, res, next) {
+    console.log(req.body.name);
+    res.render('index');
 });
 
 
